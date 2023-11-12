858e6d8b834606d31b5b41ecf18339ea3baee39e
Zohra Batool <146383893+zohra112@users.noreply.github.com>
2023-11-12 17:14:47 +0500

    Update Mountain Journey ticketing System.py

--- Mountain Journey ticketing System.py
+++ Mountain Journey ticketing System.py
@@ -1,89 +1,74 @@
 #Program to implement : A Ticketing System : Mountain Journey , An electric mountain railway
-
 import datetime
 
-# Task1: Start of the day
 journeys = {
-    "09:00": {"up": 0, "down": 0, "revenue": 0},
-    "11:00": {"up": 0, "down": 0, "revenue": 0},
-    "13:00": {"up": 0, "down": 0, "revenue": 0},
-    "15:00": {"up": 0, "down": 0, "revenue": 0}
+    "09:00": {"up": {"seats": 80, "revenue": 0}},
+    "10:00": {"down": {"seats": 80, "revenue": 0}},
+    "11:00": {"up": {"seats": 80, "revenue": 0}},
+    "12:00": {"down": {"seats": 80, "revenue": 0}},
+    "13:00": {"up": {"seats": 80, "revenue": 0}},
+    "14:00": {"down": {"seats": 80, "revenue": 0}},
+    "15:00": {"up": {"seats": 80, "revenue": 0}},
+    "16:00": {"down": {"seats": 80, "revenue": 0}}
 }
-
-# Task2: Purchasing tickets
+#purchase tickets function
 def purchase_tickets():
-    journey_time = input("Enter the journey time (in HH:MM format): ")
-    num_passengers = int(input("Enter the number of passengers: "))
-    
-    #this chunk will check if the journey time is valid
+    journey_time = input("Enter the desired shift time (in HH:MM format): ")
+
     if journey_time not in journeys.keys():
-        print("Invalid journey time.")
+        print("Invalid shift time.")
         return
-    
-    journey = journeys[journey_time]
-    
-    # To check if there are enough tickets available
-    if num_passengers * 2 > 80 * 6:
-        print("Not enough seats available.")
+
+    available_shifts = journeys[journey_time]
+    shift = list(available_shifts.keys())[0]
+    available_seats = available_shifts[shift]["seats"]
+
+    num_passengers = int(input("Enter the number of passengers: "))
+
+    if num_passengers > available_seats:
+        print(f"Not enough seats available for the {shift} shift.")
         return
-    
-    # To calculate the total price including group discount
+
     price_per_ticket = 25
     total_price = num_passengers * price_per_ticket
-    
-    if num_passengers >= 10 and num_passengers <= 80:
-        free_tickets = num_passengers // 10
-        total_price -= free_tickets * price_per_ticket
-    
-    # to update the screen display and the data for the totals
-    journey["up"] += num_passengers
+
+    journey = journeys[journey_time][shift]
+    journey["seats"] -= num_passengers
     journey["revenue"] += total_price
-    
+
     print("Tickets purchased successfully.")
     print(f"Total amount paid: ${total_price}")
 
-# Task3: End of the day report
-def end_of_day():
-    total_passengers = 0
-    total_revenue = 0
-    max_passengers = 0
-    max_journey_time = ""
-    
-    print("End of the day report:")
-    
-    for journey_time, journey in journeys.items():
-        total_passengers += journey["up"]
-        total_revenue += journey["revenue"]
-        
-        print("Journey Time:", journey_time)
-        print("Passengers:", journey["up"])
-        print("Revenue collected:", journey["revenue"])
-        print("---------------------------------")
-        
-        if journey["up"] > max_passengers:
-            max_passengers = journey["up"]
-            max_journey_time = journey_time
-    
-    print("Total passengers for the day:", total_passengers)
-    print("Total revenue collected for the day: $", total_revenue)
-    print("Journey with the most passengers:", max_journey_time, "with", max_passengers, "passengers.")
-
-# Main 
-print("Start of the day.")
-
-print("Journey times:")
-for journey_time in journeys.keys():
-    print(journey_time)
+def display_available_shifts():
+    print("Available shifts:")
+    for journey_time, journey_shifts in journeys.items():
+        for shift, shift_details in journey_shifts.items():
+            seats_available = shift_details["seats"]
+            print(f"{journey_time} ({shift.capitalize()}) : {seats_available} seats available")
+#to show end of day report
+def display_full_day_report():
+    print("Full day report:")
+    for journey_time, journey_shifts in journeys.items():
+        for shift, shift_details in journey_shifts.items():
+            seats_available = shift_details["seats"]
+            revenue = shift_details["revenue"]
+            print(f"{journey_time} ({shift.capitalize()})")
+            print(f"Seats available: {seats_available}")
+            print(f"Revenue generated: ${revenue}")
+            print()
 
 while True:
-    choice = input("Enter '1' to purchase tickets, '2' to end day, or 'q' to quit: ")
-    
+    print("Mountain Journey Ticketing System")
+    print("---------------------------------")
+    choice = input("Enter '1' to purchase tickets, '2' for full day report, or '3' to quit: ")
+
     if choice == '1':
+        display_available_shifts()
         purchase_tickets()
     elif choice == '2':
-        end_of_day()
-        break
-    elif choice.lower() == 'q':
+        print("\nStart of Day")
+        display_full_day_report()
+    elif choice == '3':
         break
     else:
         print("Invalid choice.")
